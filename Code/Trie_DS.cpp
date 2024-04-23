c114a499e12823ec0d4444d323ef438f9631ff51
Sreemanth <164031015+Sreemanth-coder@users.noreply.github.com>
2024-04-23 22:54:14 +0530

    Search Utility added

--- Code/Trie_DS.cpp
+++ Code/Trie_DS.cpp
@@ -48,6 +48,24 @@ class Trie {
         insertUtil(root,word);
 
     }
+  bool searchUtil(TrieNode* root, string word){
+      if(word.length() == 0){
+          return root->isTerminal;
+      }
+        int index = word[0] - 'A';
+        TrieNode* child;
+      if(root->children[index] !=NULL){
+          child = root->children[index];
+      }
+      else{
+          return false;
+      }
+
+      return searchUtil(child, word.substr(1));
+  }
+    bool search(string word){
+        retun searchUtil(root,word);
+    }
 
 };
 int main()
