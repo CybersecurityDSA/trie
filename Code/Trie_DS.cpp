3079214e02cde8c750b30a1bdf819d2cf161173a
Rikhil777 <kakanirikhil7@gmail.com>
2024-04-23 21:44:48 +0530

    NodeCreated

---
+++ Code/Trie_DS.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#include<stack>
+using namespace std;
+
+class TrieNode {
+    public:
+        char data;
+        TrieNode* children[26];
+        bool isTerminal;
+
+        TrieNode(char ch)
+        {
+            data = ch;
+            for(int i = 0;i<26;i++)
+            {
+                children[i] = NULL;
+            }
+            isTerminal = false;
+
+        }
+
+};
+
+
+class Trie {
+    public:
+        TrieNode* root;
+    void insertUtil(TrieNode* root, string word){
+        if(word.length() == 0){
+            root -> isTerminal;
+            return false;
+        }
+    }
+    void insertWord(string word){
+
+    }
+
+};
+int main()
+{
+
+}
