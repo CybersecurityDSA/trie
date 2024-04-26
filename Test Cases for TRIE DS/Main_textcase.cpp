00c1dcbbee79dfcefbd448aa8cf8170b23c83c07
Rikhil777 <kakanirikhil7@gmail.com>
2024-04-26 11:09:53 +0530

    TestCases_Folder added

---
+++ Test Cases for TRIE DS/Main_textcase.cpp
@@ -0,0 +1,19 @@
+#include "C:\Users\kakan\OneDrive\Documents\Trie_DS\trie\Code\Trie_DS.cpp"
+#include <cassert>
+
+int main() {
+  
+    Trie t;
+    t.insertWord("HELLO");
+    assert(t.searchWord("HELLO") == true);
+
+    t.insertWord("WORLD");
+    t.insertWord("APPLE");
+    assert(t.searchWord("WORLD") == true);
+    assert(t.searchWord("APPLE") == true);
+
+    assert(t.searchWord("FOOBAR") == false);
+
+
+    return 0;
+}
